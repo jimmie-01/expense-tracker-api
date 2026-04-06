055ffaad662ea0b0a2955cbd279515e444570bbb
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-04-06 10:58:04 +0100

    Route for updating expenses

--- databases/postgres.js
+++ databases/postgres.js
@@ -17,7 +17,7 @@ const pool = new Pool({
 router.get('/expenses', async(req, res) => {
 	try {
 		const query = "SELECT * FROM expenses";
-		const { rows } = pool.query(query);
+		const { rows } = await pool.query(query);
 		res.json(rows);
 	} catch (err) {
 		console.error(error);
@@ -25,7 +25,7 @@ router.get('/expenses', async(req, res) => {
 	}
 })
 
-// Routes to add new expenses
+// Route to add new expenses
 
 router.post('/expenses', async(req, res) => {
 	try {
@@ -37,6 +37,22 @@ router.post('/expenses', async(req, res) => {
 	} catch (err) {
 		console.error(err);
 		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+// Route to Update expenses
+
+router.put("/expenses/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const { name, amount, date } = req.body;
+		const query = "UPDATE expenses SET name = $1, amount = $2, date = $3 WHERE id = $4 RETURNING *"
+		const { rows } = await pool.query(query, [name, amount, date, id]);
+
+		res.status(201).json(rows[0]);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: "Internal server error" });
 	}
 })
 module.exports = router;
